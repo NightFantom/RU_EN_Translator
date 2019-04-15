20faa3c221ff6faa898dea6eaf83c44e11df2a94
Denis <usachevdy@yandex.ru>
2019-04-15 23:49:26 +0300

    Batch execution. Part 2

--- Main.py
+++ Main.py
@@ -11,7 +11,7 @@ BASE_PATH = "."
 
 # In[ ]:
 
-
+import sys
 import os
 import random
 import traceback
@@ -31,7 +31,7 @@ from torch import nn
 from gensim.models import FastText
 
 from tensorboardX import SummaryWriter
-from nltk.translate.bleu_score import sentence_bleu
+from nltk.translate.bleu_score import sentence_bleu, corpus_bleu
 from translator_constants.global_constant import *
 from text_utils.vocabulary import Vocabulary
 from text_utils.fast_text import FastTextWrapper
@@ -102,7 +102,7 @@ corpus_df.head()
 # In[ ]:
 
 
-corpus_df = corpus_df.iloc[:10000]
+corpus_df = corpus_df.iloc[:900]
 
 # # Convert English tokens in one hot vectors
 
@@ -201,8 +201,10 @@ class Encoder(nn.Module):
     def forward(self, X):
         """
         X.shape [batch_size, seq_len, fast_text_vect]
-        return: output.shape[1,hidden_size]
+        return: output.shape [batch_size, seq_len, hidden_size]
                 hidden_states - typle
+                    hidden_states[0].shape (1, batch_size, hidden_size)
+                    hidden_states[1].shape (1, batch_size, hidden_size)
         """
         output, hidden_states = self.encoder(X)
 
@@ -253,11 +255,12 @@ class Trainer:
                  SOS,
                  epoch,
                  device,
-                 verbose=False):
+                 verbose=False,
+                 model_save_path=None):
         self.input_size = input_size
         self.hidden_size = hidden_size
-        self.encoder = encoder
-        self.decoder = decoder
+        self.encoder: Encoder = encoder
+        self.decoder: Decoder = decoder
         self.encoder_optimizer = encoder_optimizer
         self.decoder_optimizer = decoder_optimizer
         self.loss = loss
@@ -267,6 +270,8 @@ class Trainer:
         self.epoch = epoch
         self.verbose = verbose
         self.device = device
+        self.model_save_path = model_save_path
+        self.best_loss = sys.float_info.max
 
     def train(self, dataloader, validation_dataloader):
         dataloader = self._wrap_dataloader(dataloader)
@@ -288,9 +293,15 @@ class Trainer:
 
             temp_metrics = self.validate(validation_dataloader)
 
+
             for key, val in temp_metrics.items():
                 self.log_writer.add_scalar(f"validation/{key}", val,
                                            current_epoch)
+
+            epoch_loss = temp_metrics[LOSS_VAL]
+            if current_epoch > 1 and epoch_loss < self.best_loss:
+                self.best_loss = epoch_loss
+                self.save_model(current_epoch)
 
     def process_one_pair(self, ru_vector, eng_vector):
         """
@@ -305,7 +316,8 @@ class Trainer:
 
         loss_torch = 0
 
-        Y = self.SOS
+        batch_size = X.shape[0]
+        Y = self.get_SOS_vector(batch_size)
 
         # Start from second token because we will compare prediction from token 'SOS' and next to him token
         for i in range(1, eng_vector.shape[1]):
@@ -315,7 +327,7 @@ class Trainer:
             Y, hidden_state = self.decoder(Y, hidden_state)
 
             # Check prediction of first tokens in batch
-            Y = Y.view(BATCH_SIZE, -1)
+            Y = Y.view(batch_size, -1)
             temp_loss = self.loss(Y, class_index)
             loss_torch += temp_loss
 
@@ -334,16 +346,18 @@ class Trainer:
     def validate(self, dataloader):
         bleu = 0
         loss_torch = 0
+        batch_amount = 0
         with torch.no_grad():
             for batch in dataloader:
-
+                batch_amount += 1
                 # ru_vector shape [batch_size, seq_len_1, fast_text_vect]
                 ru_vector = batch[RU_DS_LABEL]
                 # eng_vector shape [batch_size, seq_len_2, vocab_size]
                 eng_vector = batch[EN_DS_LABEL]
 
                 X, hidden_state = self.encoder(ru_vector)
-                Y = self.SOS
+                batch_size = X.shape[0]
+                Y = self.get_SOS_vector(batch_size)
 
                 sentence = []
                 target_sentence_list = []
@@ -354,42 +368,41 @@ class Trainer:
                     Y, hidden_state = self.decoder(Y, hidden_state)
 
                     # Check prediction of first tokens in batch
-                    Y = Y.view(BATCH_SIZE, -1)
+                    Y = Y.view(batch_size, -1)
                     temp_loss = self.loss(Y, class_index)
                     loss_torch += temp_loss
 
                     Y, word_index = self._get_pred_vect(Y)
 
-                    target_sentence_list.append(class_index[0].item())
+                    target_sentence_list.append(class_index)
                     sentence.append(word_index)
-                    if Y is None:
-                        break
-                bleu += sentence_bleu([sentence], target_sentence_list)
-            bleu = bleu / len(dataloader.dataset)
+
+                bleu += self.calculate_bleu(sentence, target_sentence_list)
+            bleu = bleu / batch_amount
 
         return {LOSS_VAL: loss_torch.item(), BLEU_SCORE: bleu}
 
     def predict(self, dataloader):
         dataloader = self._wrap_dataloader(dataloader)
-
+        result = []
         with torch.no_grad():
-            result = []
             for batch in dataloader:
                 sentence = []
-                result.append(sentence)
-
                 ru_vector = batch[RU_DS_LABEL]
 
                 X, hidden_state = self.encoder(ru_vector)
-                Y = self.SOS
+                batch_size = X.shape[0]
+                Y = self.get_SOS_vector(batch_size)
                 for i in range(1, ru_vector.shape[1]):
                     Y, hidden_state = self.decoder(Y, hidden_state)
-
+                    Y = Y.view(batch_size, -1)
                     Y, word_index = self._get_pred_vect(Y)
                     sentence.append(word_index)
-                    if Y is None:
-                        break
-            return result
+                sentence = self.normilize(sentence)
+                result.extend(sentence)
+        result = torch.stack(result, dim=0)
+        result = result.numpy()
+        return result
 
     def _wrap_dataloader(self, dataloader):
         if self.verbose:
@@ -398,16 +411,52 @@ class Trainer:
 
     def _get_pred_vect(self, Y):
         """
-        Y.shape (batch_size,1,vocabular_size)
+        :param Y - Y.shape (batch_size,vocabular_size)
+        :return res.shape (batch_size, 1, vocabular_size)
+                word_index.shape (batch_size, 1)
         """
         _, word_index = Y.topk(1)
-        sequence_len = Y.size()[2]
+        vocabular_size = Y.size()[1]
         batch_size = Y.size()[BATCH_SIZE_INDEX]
-        res = torch.zeros((batch_size, 1, sequence_len), device=self.device)
+        res = torch.zeros((batch_size, 1, vocabular_size), device=self.device)
         for i in range(0, word_index.shape[0]):
             res[i, 0, word_index[i]] = 1
+        word_index = word_index.view(-1)
         return res, word_index
 
+    def calculate_bleu(self, predicted_sentence, target_sentence_list):
+        target_sentence_torch = self.normilize(target_sentence_list)
+        predicted_sentence_torch = self.normilize(predicted_sentence)
+        referenses = []
+        for row in predicted_sentence_torch:
+            referenses.append([row])
+        bleu = corpus_bleu(referenses, target_sentence_torch)
+        return bleu
+
+    def normilize(self, matrix_list):
+        """
+        Because of rows are position of word in sentence and columns are sentence we transpose matrix_list
+        :param matrix_list: matrix, column - sentence, row - word in sentence
+        :return: matrix where column - word in sentence, row - sentence
+        """
+        matrix_tensor = torch.stack(matrix_list, dim=0)
+        matrix_tensor = torch.transpose(matrix_tensor, 0, 1)
+        return matrix_tensor
+
+    def save_model(self, epoch):
+        directory = os.path.join(self.model_save_path, f"model_{epoch}")
+        os.makedirs(directory)
+        encoder_path = os.path.join(directory, "encoder.pt")
+        torch.save(self.encoder.state_dict(), encoder_path)
+
+        decoder_path = os.path.join(directory, "decoder.pt")
+        torch.save(self.decoder.state_dict(), decoder_path)
+
+    def get_SOS_vector(self, batch_size):
+        vector = self.SOS
+        if vector.shape[0] > batch_size:
+            vector = vector[:batch_size]
+        return vector
 
 # In[ ]:
 
@@ -446,8 +495,8 @@ experiment_number = 0
 
 
 experiment_number += 1
-# log_path = os.path.join(TENSORBOARD_LOG, f"test_{experiment_number}")
-log_path = os.path.join(TENSORBOARD_LOG, f"test_tanh")
+log_path = os.path.join(TENSORBOARD_LOG, f"test_{experiment_number}")
+# log_path = os.path.join(TENSORBOARD_LOG, f"test_tanh")
 
 log_writer = SummaryWriter(log_path)
 
@@ -463,7 +512,8 @@ trainer = Trainer(log_writer=log_writer,
                   SOS=SOS_vector,
                   epoch=5,
                   device=device,
-                  verbose=False)
+                  verbose=False,
+                  model_save_path="./models")
 
 # In[ ]:
 
